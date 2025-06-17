6d835be317551f445a9d3e4ff65a8da298c21f89
Gaurav Tanpure <gauravtanpure999@gmail.com>
2025-06-17 15:51:40 +0530

    Warning Banner Add

--- WABiz-App/src/App.jsx
+++ WABiz-App/src/App.jsx
@@ -2,11 +2,13 @@ import Welcome from './components/Welcome'
 import './App.css'
 import Pricing from './components/Pricing';
 import Footer from './components/Footer';
+import WarningBanner from './components/WarningBanner';
 
 function App() {
   return (
     <>
     <Welcome />
+    <WarningBanner />
     <Pricing />
     <Footer />
     </>
