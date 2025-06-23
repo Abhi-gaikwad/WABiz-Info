b78d856dfa09b1b6f32734f14859eedc6fcb6974
Gaurav Tanpure <gauravtanpure999@gmail.com>
2025-06-23 12:03:06 +0530

    UTM Added

--- WABiz-App/src/App.jsx
+++ WABiz-App/src/App.jsx
@@ -1,31 +1,52 @@
-import Welcome from './components/Welcome'
-import './App.css'
+import { useEffect } from 'react';
+import Welcome from './components/Welcome';
+import './App.css';
 import Pricing from './components/Pricing';
 import Footer from './components/Footer';
 import WarningBanner from './components/WarningBanner';
-import Navbar from './components/Navbar'; // Assuming you have a Navbar component
+import Navbar from './components/Navbar';
 import DealCard from './components/DealCard';
-import WabizFeatures from './components/WabizFeatures'; // Assuming you have a WabizFeatures component
+import WabizFeatures from './components/WabizFeatures';
 
 function App() {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    let source = urlParams.get('source');
+
+    if (!source) {
+      const ref = document.referrer;
+      if (ref.includes("instagram.com")) source = "instagram";
+      else if (ref.includes("facebook.com")) source = "facebook";
+      else if (ref.includes("t.co")) source = "twitter";
+      else if (ref.includes("linkedin.com")) source = "linkedin";
+      else if (ref.includes("whatsapp.com")) source = "whatsapp";
+    }
+
+    if (source) {
+      console.log("Traffic source:", source);
+      // Optional: Store in localStorage or send to backend
+      // localStorage.setItem("trafficSource", source);
+    }
+  }, []);
+
   return (
     <>
-    <Navbar />
-    <div id="welcome">
-      <Welcome />
-    </div>
+      <Navbar />
+      <div id="welcome">
+        <Welcome />
+      </div>
 
-    <WarningBanner />
-    <WabizFeatures />
-    <DealCard />
+      <WarningBanner />
+      <WabizFeatures />
+      <DealCard />
 
-    <div id='pricing'>
-    <Pricing />
-    </div>
+      <div id='pricing'>
+        <Pricing />
+      </div>
 
-    <Footer />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
